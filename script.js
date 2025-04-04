1f9dbfabc634ecb9081536d415e1b8148c1f8490
dangpham123-vn <dpham6708@gmail.com>
2025-04-04 09:40:25 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u=un.value
+  let p=pw.value
+  if (u=="Q" && p=="1")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
